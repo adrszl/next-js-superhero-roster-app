c48784d84fcac14c875288b8ae260a4db678c0ae
Adrian Szlegel <adrian95999@gmail.com>
2026-04-22 13:03:01 +0200

    name fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { CharactersList } from '@/components/CharactersList';
 import { LocalCharacter } from '@/types/character';
 
 async function getLocalCharacters(): Promise<LocalCharacter[]> {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/characters`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/characters`, {
     cache: 'no-store',
   });
 
